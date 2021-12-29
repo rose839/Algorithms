b70682d463f812ad09ee994cbe439c472639bbdd
rose839 <617418745@qq.com>
2021-12-29 15:57:44 +0800

    feat(bitmap): add go implemention

---
+++ BitMap/go/bitmap_test.go
@@ -0,0 +1,18 @@
+package bigmap
+
+import (
+	"testing"
+
+	"gotest.tools/v3/assert"
+)
+
+func TestBitMap(t *testing.T) {
+	bitmap := New(80)
+	assert.Equal(t, len(bitmap), 11)
+
+	bitmap.Set(77)
+	assert.Equal(t, bitmap.Get(77), true)
+
+	assert.Equal(t, bitmap.Get(78), false)
+	assert.Equal(t, bitmap.Get(100), false)
+}
---
+++ BitMap/go/go.mod
@@ -0,0 +1,5 @@
+module github.com/rose839/Algorithms/BitMap/go
+
+go 1.16
+
+require gotest.tools/v3 v3.0.3
